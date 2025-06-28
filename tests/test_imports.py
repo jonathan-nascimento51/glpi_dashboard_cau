267b5d173d39cd8bcfb6c57b6b4b16d001b0255d
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2025-06-28 03:20:52 -0300

    Move import chec ks and remove old test dir

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,11 @@
 import importlib
 
 
+def test_import_dash_layout():
+    module = importlib.import_module("glpi_dashboard.dashboard.layout")
+    assert module is not None
+
+
 def test_import_cli_groups():
     module = importlib.import_module("glpi_dashboard.cli.tickets_groups")
     assert module is not None
