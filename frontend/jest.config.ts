a84b838fe153361e7eaa51d72f2726389d643e82
Jonathan Nascimento <jonathanmoletta2010@hotmail.com>
2025-07-02 13:16:03 -0300

    feat(frontend): add jest config and sample test

---
+++ frontend/jest.config.ts
@@ -0,0 +1,23 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({ dir: './' })
+
+const config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+export default createJestConfig(config)
