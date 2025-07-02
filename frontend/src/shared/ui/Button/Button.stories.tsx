39765ace94a68cff14bab0a82f04573a2ad9cf00
Jonathan Nascimento <jonathanmoletta2010@hotmail.com>
2025-07-02 13:03:31 -0300

    chore(storybook ): add basic configuration and example

---
+++ frontend/src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+
+const meta: Meta<typeof Button> = {
+  title: 'shared/Button',
+  component: Button,
+  args: {
+    children: 'Click me',
+  },
+  argTypes: {
+    onClick: { action: 'clicked' },
+    disabled: { control: 'boolean' },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {};
