f4419bbfe17d6563480c24c5e6e761df096a5391
Jonathan Nascimento <jonathanmoletta2010@hotmail.com>
2025-07-02 18:44:13 -0300

    tentando rodar o front

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import React from 'react';
 import "./globals.css";
 import { ReactQueryProvider } from './providers/ReactQueryProvider';
 
-const geistSans = Geist({
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
