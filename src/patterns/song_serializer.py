824a6ad7e6fac0378d48b6745db9ce659aa876fe
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2025-07-03 05:17:48 -0300

    Remove unused im ports

--- src/patterns/song_serializer.py
+++ src/patterns/song_serializer.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Protocol, Callable
+from typing import Protocol
 import json
 import xml.etree.ElementTree as ET
 import yaml
