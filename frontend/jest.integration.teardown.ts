df701d0310dab9f8e33add58eac6ea2bd9562aa4
Jonathan Nascimento <jonathanmoletta2010@hotmail.com>
2025-07-08 11:59:04 -0300

    fix: adjust tsco nfig and jest integration helpers

--- frontend/jest.integration.teardown.ts
+++ frontend/jest.integration.teardown.ts
@@ -1,4 +1,4 @@
-import { Docker } from 'testcontainers'
+import { dockerClient } from 'testcontainers'
 import fs from 'fs'
 import path from 'path'
 
@@ -6,8 +6,8 @@ export default async function globalTeardown() {
   const idFile = path.join(__dirname, 'container-id')
   if (fs.existsSync(idFile)) {
     const id = fs.readFileSync(idFile, 'utf-8').trim()
-    const docker = new Docker()
-    await docker.getContainer(id).stop()
+    const { dockerode } = await dockerClient()
+    await dockerode.getContainer(id).stop()
     fs.unlinkSync(idFile)
   }
 }
