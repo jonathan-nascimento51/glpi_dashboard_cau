0c6bc9c3ee912d2788d66f8cb321b92d47fcc777
Jonathan Nascimento <jonathanmoletta2010@hotmail.com>
2025-07-08 11:59:04 -0300

    fix: adjust tsco nfig and jest integration helpers

--- frontend/jest.integration.setup.ts
+++ frontend/jest.integration.setup.ts
@@ -8,10 +8,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default async function globalSetup() {
   const context = path.resolve(__dirname, '..')
-  const image = await GenericContainer.fromDockerfile(context).build()
-  const container = await new GenericContainer(image)
-    .withExposedPorts(8000)
-    .start()
+  const container = await GenericContainer.fromDockerfile(context)
+    .build()
+    .then((image) => image.withExposedPorts(8000).start())
+  // StartedTestContainer is returned above
 
   const mappedPort = container.getMappedPort(8000)
   process.env.NEXT_PUBLIC_API_BASE_URL = `http://localhost:${mappedPort}`
