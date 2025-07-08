42bb7b222fe84c6529ef51763a9c34b634fd4357
Jonathan Nascimento <jonathanmoletta2010@hotmail.com>
2025-07-08 13:33:40 -0300

    chore(frontend):  add tsconfig paths plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
   resolve: {
     alias: {
