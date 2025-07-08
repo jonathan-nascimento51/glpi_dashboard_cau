957f1ce632ee73876862f4b875cd8f14e5bd88d2
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2025-07-08 01:20:37 -0300

    chore(db): add r oles and update docs

--- docker/db-init/01-create-roles.sql
+++ docker/db-init/01-create-roles.sql
@@ -1,40 +1,26 @@
--- Role definitions for GLPI Dashboard database
--- migration_user: used by database migrations to alter schema objects
--- app_readwrite: read/write access for application runtime
--- app_readonly: read-only access for reporting and analytics
-
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'migration_user') THEN
-        CREATE ROLE migration_user;
-    END IF;
-END$$;
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'migration_user') THEN
+      CREATE ROLE migration_user NOLOGIN;
+   END IF;
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'app_readwrite') THEN
+      CREATE ROLE app_readwrite NOLOGIN;
+   END IF;
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'app_readonly') THEN
+      CREATE ROLE app_readonly NOLOGIN;
+   END IF;
+END
+$$;
 
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'app_readwrite') THEN
-        CREATE ROLE app_readwrite;
-    END IF;
-END$$;
-
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'app_readonly') THEN
-        CREATE ROLE app_readonly;
-    END IF;
-END$$;
-
--- assign privileges to application user
+-- Grant the application login user membership in the read/write role
 GRANT app_readwrite TO "user";
-GRANT migration_user TO "user";
 
--- allow migration role to create objects
-GRANT CREATE, USAGE ON SCHEMA public TO migration_user;
-
--- grant privileges on existing tables
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app_readwrite;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO app_readonly;
-
--- ensure future tables inherit privileges
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_readwrite;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO app_readonly;
+-- Tables created by migration_user should be accessible to application roles
+ALTER DEFAULT PRIVILEGES FOR ROLE migration_user
+   GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_readwrite;
+ALTER DEFAULT PRIVILEGES FOR ROLE migration_user
+   GRANT SELECT ON TABLES TO app_readonly;
+ALTER DEFAULT PRIVILEGES FOR ROLE migration_user
+   GRANT USAGE, SELECT ON SEQUENCES TO app_readwrite;
+ALTER DEFAULT PRIVILEGES FOR ROLE migration_user
+   GRANT SELECT ON SEQUENCES TO app_readonly;
