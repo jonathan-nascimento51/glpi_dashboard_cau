1b5017edcb0f9334b3e76f6b7ae6c4512ff9142a
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2025-07-09 22:37:44 -0300

    fix: noninteractive docker gpg import

--- setup.sh
+++ setup.sh
@@ -71,7 +71,8 @@ echo ">>> (6/6) Instalando Docker..."
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 sudo apt-get update && sudo apt-get install curl
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+  | sudo gpg --dearmor --batch --yes -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 curl -fsSL https://get.docker.com | sudo sh
