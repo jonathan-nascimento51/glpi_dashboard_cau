f878fc125d41a420e04933654b241908bace0991
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2025-07-10 00:29:19 -0300

    chore(frontend):  consolidate eslint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,23 +1,45 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintImport from 'eslint-plugin-import'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
+    ignores: ['node_modules/', 'out/'],
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.flat.recommended,
+      eslintImport.flatConfigs.recommended,
+      eslintImport.flatConfigs.typescript,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
       globals: globals.browser,
+    },
+    rules: {
+      'import/no-restricted-paths': [
+        'error',
+        {
+          zones: [
+            { target: './src/features', from: './src/widgets' },
+            { target: './src/entities', from: './src/features' },
+            { target: './src/entities', from: './src/widgets' },
+          ],
+        },
+      ],
     },
   },
 ])
