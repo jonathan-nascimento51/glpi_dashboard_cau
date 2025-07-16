5a7370f050c7e4d581205aa88a8fe399b72f1f51
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2025-07-16 18:46:35 -0300

    corrijindo dependencias

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/frontend/react_app/src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
