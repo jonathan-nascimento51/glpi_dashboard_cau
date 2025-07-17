442cad07f0ea353b44bf26a89d9de62624b62ebe
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2025-07-16 23:15:49 -0300

    convertendo terminadores

--- src/frontend/react_app/postcss.config.js
+++ src/frontend/react_app/postcss.config.js
@@ -1,6 +1,7 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+// postcss.config.js (correto)
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer')
+  ]
+}
