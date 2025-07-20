b42d520782c5e27b325387e51ed040002948676f
Jonathan Nascimento Moletta <jonathanmoletta2010@hotmail.com>
2025-07-20 01:55:44 -0300

    chore(frontend): remove js artifacts

--- src/frontend/react_app/src/shared/index.js
+++
@@ -1,2 +0,0 @@
-export * from './ui/Button/Button.js';
-export * from './ui/Input/Input.js';
--- src/frontend/react_app/src/shared/ui/Input/Input.stories.js
+++
@@ -1,8 +0,0 @@
-import { Input } from './Input.js';
-const meta = {
-    title: 'shared/Input',
-    component: Input,
-    args: { placeholder: 'Type here' },
-};
-export default meta;
-export const Primary = {};
