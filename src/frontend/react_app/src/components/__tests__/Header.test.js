29dae3278c598a43d8f6c76775dbb5e9aeff632c
Jonathan Nascimento Moletta <jonathanmoletta2010@hotmail.com>
2025-07-20 00:17:57 -0300

    chore(frontend): remove js component artifacts

--- src/frontend/react_app/src/components/__tests__/Header.test.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { render, screen } from '@testing-library/react';
-import Header from '../Header.js';
-describe('Header', () => {
-    it('renders brand title', () => {
-        render(_jsx(Header, {}));
-        expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Centro de Comando');
-    });
-});
