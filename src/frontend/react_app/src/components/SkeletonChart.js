8d157784bb621eafcf78bcbc3d8ffa53180fffc5
Jonathan Nascimento Moletta <jonathanmoletta2010@hotmail.com>
2025-07-20 00:17:57 -0300

    chore(frontend): remove js component artifacts

--- src/frontend/react_app/src/components/SkeletonChart.js
+++
@@ -1,4 +0,0 @@
-"use client";
-import { jsx as _jsx } from "react/jsx-runtime";
-const SkeletonChart = ({ heightClass = 'h-[300px]', }) => (_jsx("div", { className: "bg-white dark:bg-gray-800 p-4 rounded shadow w-full", children: _jsx("div", { className: `skeleton w-full rounded ${heightClass}` }) }));
-export default SkeletonChart;
