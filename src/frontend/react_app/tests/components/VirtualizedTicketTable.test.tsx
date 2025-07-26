1586e0876f8b81faf918d243adc4b797ef07bb61
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2025-07-26 01:45:57 -0300

    correçoes apresentada nos testes

--- src/frontend/react_app/tests/components/VirtualizedTicketTable.test.tsx
+++ src/frontend/react_app/tests/components/VirtualizedTicketTable.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { render, screen, fireEvent, waitFor } from '@testing-library/react'
 import { VirtualizedTicketTable } from '@/components/VirtualizedTicketTable'
 import { jest } from '@jest/globals'
